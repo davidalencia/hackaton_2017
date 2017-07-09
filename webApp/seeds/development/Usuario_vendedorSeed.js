7bb47000a14112285b1abb5a064a60251f45a2d1
rubal501 <rubal820@gmail.com>
2017-07-08 21:45:02 -0500

    se agrego la seed de vendedor

--- webApp/seeds/development/Usuario_vendedorSeed.js
+++ webApp/seeds/development/Usuario_vendedorSeed.js
@@ -3,6 +3,29 @@
 
 module.exports=[
   {
-    Pais: 'Mexico',
+    Usuario_vendedor:{
+      type:"string",
+      required:true,
+      unique: true,
+    },
+    Nombre:{
+      type: "string",
+      required: true,
+    },
+    Apellido:{
+      type: "string",
+      required: true//no estoy seguro
+    },
+    Telefono:"",
+
+    Correo:"",
+
+    Contrasegna:"",
+
+    Codigo_postal:"",
+    Calle:"",
+    Num_ext :
+    },
+    Productos:,
   }
 ]
